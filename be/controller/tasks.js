9d733449e81befe3c7f96f4c7480c041119b6e30
asad-leghari <asadleghari537@gmail.com>
2025-04-19 18:48:01 +0500

    Enhance task and user controllers with async functionality; implement task creation and user retrieval with error handling.

--- be/controller/tasks.js
+++ be/controller/tasks.js
@@ -1,15 +1,29 @@
+const Model = require("../model/tasks");
+
 const getAllTasks = (req, res) => {
   res.status(200).json("hello");
 };
-const createTask = (req, res) => {
-  res.status(201).json("hello");
+
+const createTask = async (req, res) => {
+  try {
+    const data = req.body;
+    const create_task = await Model.create(data);
+    const task = await create_task.save();
+    res.status(201).json(task);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json("something went wrong");
+  }
 };
+
 const getSingleTask = (req, res) => {
   res.status(200).json("hello");
 };
+
 const deleteTask = (req, res) => {
   res.status(200).json("hello");
 };
+
 const updateTask = (req, res) => {
   res.status(200).json("hello");
 };
--- be/controller/users.js
+++ be/controller/users.js
@@ -22,8 +22,17 @@ const createUser = async (req, res) => {
   }
 };
 
-const getUser = (req, res) => {
-  res.status(200).json("user");
+const getUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await Model.findById(id).populate("tasks");
+    if (!user) {
+      return res.status(404).json({ msg: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const updateUser = (req, res) => {
